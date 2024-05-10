b45d91fe675eaba1b0f0b19e092575b2232c355c
David Wang <wangd7@bxscience.edu>
2024-05-10 11:08:08 -0400

    feat: Added controls UI

--- Services/Multiplayer/ConnectionMenu.cs
+++ Services/Multiplayer/ConnectionMenu.cs
@@ -3,53 +3,53 @@ using System;
 
 public partial class ConnectionMenu : Control
 {
-    [Signal]
-    public delegate void OnHostEventHandler();
+	[Signal]
+	public delegate void OnHostEventHandler();
 
-    [Signal]
-    public delegate void OnJoinEventHandler(String code);
+	[Signal]
+	public delegate void OnJoinEventHandler(String code);
 
-    [Signal]
-    public delegate void onHostLocalEventHandler();
+	[Signal]
+	public delegate void onHostLocalEventHandler();
 
-    [Signal]
-    public delegate void onJoinLocalEventHandler(String ip);
+	[Signal]
+	public delegate void onJoinLocalEventHandler(String ip);
 
-    // Attempt to host a server
-    public void OnHostPressed()
-    {
-        EmitSignal(SignalName.OnHost);
-    }
+	// Attempt to host a server
+	public void OnHostPressed()
+	{
+		EmitSignal(SignalName.OnHost);
+	}
 
-    public void onHostLocalPressed(){
-        EmitSignal(SignalName.onHostLocal);
-    }
+	public void onHostLocalPressed(){
+		EmitSignal(SignalName.onHostLocal);
+	}
 
-    public void onJoinLocalPressed(string ip){
-        GD.Print("attempt local join");
-        EmitSignal(SignalName.onJoinLocal, ip);
-    }
-
-
-    public void openLobby()
-    {
-        Visible = false ;
-    }
-    
+	public void onJoinLocalPressed(string ip){
+		GD.Print("attempt local join");
+		EmitSignal(SignalName.onJoinLocal, ip);
+	}
 
 
+	public void openLobby()
+	{
+		Visible = false ;
+	}
+	
 
-    public void OnJoinPressed()
-    {
-        // Get the code from the input field
-        LineEdit codeInput = GetNode<LineEdit>("CodeInput");
-        String code = codeInput.Text;
-        GD.Print(code);
-        EmitSignal(SignalName.OnJoin, code);
-    }
 
-    public void onSuccessfulConnection()
-    {
-        Visible = false;
-    }
+
+	public void OnJoinPressed()
+	{
+		// Get the code from the input field
+		LineEdit codeInput = GetNode<LineEdit>("CodeInput");
+		String code = codeInput.Text;
+		GD.Print(code);
+		EmitSignal(SignalName.OnJoin, code);
+	}
+
+	public void onSuccessfulConnection()
+	{
+		Visible = false;
+	}
 }
