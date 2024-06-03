77e7c13f33d5e2426fb7582b1804303b47beaffe
David Wang <wangd7@bxscience.edu>
2024-06-03 10:33:32 -0400

    feat: Worked on shop UI

--- Services/Generator/Generator.cs
+++ Services/Generator/Generator.cs
@@ -4,57 +4,57 @@ using System.Collections.Generic;
 using Godot;
 
 public partial class Generator {
-    public static Stack<Room> rooms = new Stack<Room>();
+	public static Stack<Room> rooms = new Stack<Room>();
 
-    public static void generateRooms(int rooms) {
-        for (int i = 0; i < rooms; i++) {
-            if (calculateBoss(i)) {
-                addBoss();
-                continue;
-            };
+	public static void generateRooms(int rooms) {
+		for (int i = 0; i < rooms; i++) {
+			if (calculateBoss(i)) {
+				addBoss();
+				continue;
+			};
 
-            if (calculateStair()) {
-                addStairUp();
-                continue;
-            };
+			if (calculateStair()) {
+				addStairUp();
+				continue;
+			};
 
-            if (calculateStair()) {
-                addStairDown();
-                continue;
-            };
+			if (calculateStair()) {
+				addStairDown();
+				continue;
+			};
 
-            addRoom();
-        }
-    }
+			addRoom();
+		}
+	}
 
-    public static void addStairUp() {
+	public static void addStairUp() {
 
-    }
+	}
 
-    public static void addStairDown() {
-        
-    }
+	public static void addStairDown() {
+		
+	}
 
-    public static void addBoss() {
-        
-    }
+	public static void addBoss() {
+		
+	}
 
-    public static void addRoom() {
-        
-    }
+	public static void addRoom() {
+		
+	}
 
-    public static bool calculateBoss(int number) {
-        number -= 10;
-        float random = new RandomNumberGenerator().Randf() * 2000000;
-        float chance = (float) Math.Pow(Math.E, (double) number); 
+	public static bool calculateBoss(int number) {
+		number -= 10;
+		float random = new RandomNumberGenerator().Randf() * 2000000;
+		float chance = (float) Math.Pow(Math.E, (double) number); 
 
-        if (number < 0) return false;
-        return (random > chance);
-    }
+		if (number < 0) return false;
+		return (random > chance);
+	}
 
-    public static bool calculateStair() {
-        float random = new RandomNumberGenerator().Randf();
+	public static bool calculateStair() {
+		float random = new RandomNumberGenerator().Randf();
 
-        return (random > 0.05);
-    }
+		return (random > 0.05);
+	}
 }
--- Services/Shop/Shop.cs
+++ Services/Shop/Shop.cs
@@ -4,85 +4,85 @@ using System.Collections;
 
 public partial class Shop : Control
 {
-    ItemList itemList;
+	ItemList itemList;
 
-    [Export]
-    PackedScene statItemScene;
+	[Export]
+	PackedScene statItemScene;
 
-    [Export]
-    bool testMode = false;
+	[Export]
+	bool testMode = false;
 
-    [Export]
-    int numOptions = 4;
+	[Export]
+	int numOptions = 4;
 
-    StatItem[] items;
+	StatItem[] items;
 
-    bool isActive = false;
+	bool isActive = false;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        itemList = GetNode<ItemList>("ItemList");
-        refreshShop();
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		itemList = GetNode<ItemList>("ItemList");
+		refreshShop();
+	}
 
-    public void refreshButton()
-    {
-        if (testMode)
-        {
-            refreshShop();
-            return;
-        }
-        CurrencyManager currencyManager = GetNode<CurrencyManager>("/root/CurrencyManager");
-        if (currencyManager.SpendCoins(1))
-        {
-            refreshShop();
-        }
-    }
+	public void refreshButton()
+	{
+		if (testMode)
+		{
+			refreshShop();
+			return;
+		}
+		CurrencyManager currencyManager = GetNode<CurrencyManager>("/root/CurrencyManager");
+		if (currencyManager.SpendCoins(1))
+		{
+			refreshShop();
+		}
+	}
 
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if (@event.IsActionPressed("open_shop"))
-        {
-            isActive = !isActive;
-            Visible = isActive;
-        }
-    }
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event.IsActionPressed("open_shop"))
+		{
+			isActive = !isActive;
+			Visible = isActive;
+		}
+	}
 
-    void refreshShop()
-    {
-        items = new StatItem[numOptions];
-        itemList.Clear();
-        for (int i = 0; i < numOptions; i++)
-        {
-            StatItem item = new StatItem();
-            items[i] = item;
-            if (item.value > 0)
-                itemList.AddItem(item.ToString());
-        }
-    }
+	void refreshShop()
+	{
+		items = new StatItem[numOptions];
+		itemList.Clear();
+		for (int i = 0; i < numOptions; i++)
+		{
+			StatItem item = new StatItem();
+			items[i] = item;
+			if (item.value > 0)
+				itemList.AddItem(item.ToString());
+		}
+	}
 
-    public void buyItem()
-    {
-        int index = itemList.GetSelectedItems()[0];
-        if (index < 0 || index >= items.Length)
-        {
-            return;
-        }
-        CurrencyManager currencyManager = GetNode<CurrencyManager>("/root/CurrencyManager");
-        if (currencyManager.SpendCoins(items[index].price))
-        {
-            StatItem item = items[index];
+	public void buyItem()
+	{
+		int index = itemList.GetSelectedItems()[0];
+		if (index < 0 || index >= items.Length)
+		{
+			return;
+		}
+		CurrencyManager currencyManager = GetNode<CurrencyManager>("/root/CurrencyManager");
+		if (currencyManager.SpendCoins(items[index].price))
+		{
+			StatItem item = items[index];
 
-            item.OnBuy(
-                // how do i even get the player
-                GetTree().Root
-                    .GetNode("Game")
-                    .GetNode<Player>("Player" + GetMultiplayerAuthority())
-            );
-        }
-    }
+			item.OnBuy(
+				// how do i even get the player
+				GetTree().Root
+					.GetNode("Game")
+					.GetNode<Player>("Player" + GetMultiplayerAuthority())
+			);
+		}
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) { }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) { }
 }
